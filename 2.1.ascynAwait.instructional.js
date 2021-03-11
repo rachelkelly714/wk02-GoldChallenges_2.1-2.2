bf6f8fce4bcfe9aa851c8a54ba6c68de1ddd79ab
rachelkelly714 <78329696+rachelkelly714@users.noreply.github.com>
2021-03-11 11:21:11 -0500

    Add files via upload

--- 2.1.ascynAwait.instructional.js
+++ 2.1.ascynAwait.instructional.js
@@ -5,4 +5,29 @@
         - console.log's the results of the fetch before console.logging a message saying 'this should print last'
 */
 
+
 //NOTE: Write your code below and push back to your github branch.  SUBMIT YOUR GITHUB URL IN CANVAS
+
+let mainUrl = 'https://swapi.co/api/people/3'
+
+let promise = fetch(mainUrl);
+
+fetch(mainUrl)
+.then(res => res.json())
+.then(json => console.log(json));
+
+function fetchData() {
+   fetch(mainURL)
+.then(res => res.json())
+.then(json => console.log(json));
+
+}
+
+fetchData();
+
+async function fetchData(){
+    const response = await fetch(mainURL);
+    const json= await response.json(); 
+    //console.log('Async/Await: ', json);
+
+    console.log(fetchData('This should print last.'));
